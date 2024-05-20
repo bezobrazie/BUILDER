9e0012d24bae57000b9fa31ab7c84d5ab1868b84
bezobrazieq <you@example.com>
2024-05-20 11:13:20 +0300

    fixes after review

--- coffee_builder.py
+++ coffee_builder.py
@@ -1,3 +1,4 @@
+from main import Coffee
 
 
 class CoffeeBuilder:
@@ -6,55 +7,44 @@ class CoffeeBuilder:
         self.reset()
 
     def reset(self) -> None:
-        self._coffee = Coffee()
+        self._coffee = {}
 
     def create_coffee(self):
         coffee = self._coffee
         self.reset()
-        return coffee
+        return Coffee(**coffee)
 
     def choose_sort(self, sort: str):
-        self._coffee.add(key='sort', value=sort)
+        self.add(key='sort', value=sort)
         return self
 
-    def sugar(self, sugar: int):
-        self._coffee.add(key='sugar', value=sugar)
+    def add_sugar(self, sugar: int):
+        self.add(key='sugar', value=sugar)
         return self
 
-    def syrup(self, syrup: list):
-        self._coffee.add(key='syrup', value=syrup)
+    def add_syrup(self, syrup: list):
+        self.add(key='syrup', value=syrup)
         return self
 
-    def milk(self, milk: list):
-        self._coffee.add(key='milk', value=milk)
+    def add_milk(self, milk: list):
+        self.add(key='milk', value=milk)
         return self
-
-
-class Coffee:
-
-    def __init__(self) -> None:
-        self.parts = {}
 
     def add(self, key, value) -> None:
-        if key in self.parts:
-            self.parts[key] = self.parts[key] + value
+        if key in self._coffee:
+            self._coffee[key] = self._coffee[key] + value
             return
-        self.parts[key] = value
-
-    def list_parts(self) -> None:
-        # TODO: сюда нужно запихнуть превращение self.parts в Coffee dataclass
-        print(self.parts)
+        self._coffee[key] = value
 
 
 if __name__ == "__main__":
     # coffee 1
     builder = CoffeeBuilder()
-    builder.choose_sort('ЛАТТЭ').sugar(1).sugar(5).milk([1.1]).milk([0.0]).syrup(['vkus penisa']).syrup(['kokos']).create_coffee().list_parts()
+    print(builder.choose_sort('ЛАТТЭ').add_sugar(1).add_sugar(5).add_milk([1.1]).add_milk([0.0]).add_syrup(['vkus penisa']).add_syrup(['kokos']).create_coffee())
+
     # coffee 2
     builder.choose_sort('КАПУЧИНО')
-    builder.milk([100.1])
+    builder.add_milk([100.1])
+    builder.add_syrup(['cocount'])
     coffee2 = builder.create_coffee()
-    coffee2.list_parts()
-
-
-
+    print(coffee2)
